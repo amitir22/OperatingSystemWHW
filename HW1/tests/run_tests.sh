c5387acb5b239fd3a833eb5fc115bb5c36b83459
amit <amitir22@gmail.com>
2021-05-04 19:37:23 +0000

    updated git ignore added run-tests

--- HW1/tests/run_tests.sh
+++ HW1/tests/run_tests.sh
@@ -3,5 +3,22 @@
 smash_exe_path='../smash'
 
 for input_f in *.in; do
-  echo "$input_f"
+  output_path="${input_f%%.*}.out"
+  expected_path="${input_f%%.*}.exp.processed"
+
+  #echo "$input_f"
+  #echo $output_path
+  #echo $expected_path
+
+  command="$smash_exe_path < $input_f > $output_path"
+
+  eval $command
+
+  DIFF=$(diff $output_path $expected_path)
+
+  if [$DIFF -ne 0]; then
+    echo 'Success'
+  else
+    echo 'Failed'
+  fi
 done
