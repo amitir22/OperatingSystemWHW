95d64db0f202e3111efb3b107a311541ab0c6d04
amit <amitir22@gmail.com>
2021-05-26 00:42:38 +0000

    removed redundant newlines

--- HW2/include/linux/syscalls.h
+++ HW2/include/linux/syscalls.h
@@ -940,15 +940,10 @@ asmlinkage long sys_pkey_alloc(unsigned long flags, unsigned long init_val);
 asmlinkage long sys_pkey_free(int pkey);
 asmlinkage long sys_statx(int dfd, const char __user *path, unsigned flags,
 			  unsigned mask, struct statx __user *buffer);
-			  
 asmlinkage long sys_hello(void);
-
 asmlinkage long sys_set_weight(int weight);
-
 asmlinkage long sys_get_weight(void);
-
 asmlinkage long sys_get_children_sum(void);
-
 asmlinkage long sys_get_heaviest_ancestor(void);
 
 #endif
