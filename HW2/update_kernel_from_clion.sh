4331f43d70718f77100a2acb4ae62307797a702d
amit <amitir22@gmail.com>
2021-05-26 01:31:23 +0000

    updated script

--- HW2/update_kernel_from_clion.sh
+++ HW2/update_kernel_from_clion.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-CLION_PATH='~/?/OperationSystemsWHW'
+CLION_PATH='~/CLionProjects/OperatingSystemWHW/HW2'
 
 echo '>> installing dos2unix'
 sudo apt install -y dos2unix
 echo '>> dos2unix installed.'
 
-dos2unix '~/$(CLION_PATH)/HW2/arch/x86/entry/syscalls/syscall_64.tbl'
-dos2unix '~/$(CLION_PATH)/HW2/include/linux/init_task.h'
-dos2unix '~/$(CLION_PATH)/HW2/include/linux/sched.h'
-dos2unix '~/$(CLION_PATH)/HW2/include/linux/syscalls.h'
-dos2unix '~/$(CLION_PATH)/HW2/kernel/Makefile'
-dos2unix '~/$(CLION_PATH)/HW2/kernel/hw2.c'
+dos2unix '~/$(CLION_PATH)/arch/x86/entry/syscalls/syscall_64.tbl'
+dos2unix '~/$(CLION_PATH)/include/linux/init_task.h'
+dos2unix '~/$(CLION_PATH)/include/linux/sched.h'
+dos2unix '~/$(CLION_PATH)/include/linux/syscalls.h'
+dos2unix '~/$(CLION_PATH)/kernel/Makefile'
+dos2unix '~/$(CLION_PATH)/kernel/hw2.c'
 
 echo '>> finished unixing all source files'
 
 cd ~/linux-4.15.18-custom/
+pwd
 
 echo ''
 echo '>> copying source files from clion folder to custom kernel folder:'
 
-cp '$(CLION_PATH)/HW2/arch/x86/entry/syscalls/syscall_64.tbl' arch/x86/entry/syscalls/syscall_64.tbl
-cp '$(CLION_PATH)/HW2/include/linux/init_task.h' include/linux/init_task.h
-cp '$(CLION_PATH)/HW2/include/linux/sched.h' include/linux/sched.h
-cp '$(CLION_PATH)/HW2/include/linux/syscalls.h' include/linux/syscalls.h
-cp '$(CLION_PATH)/HW2/kernel/Makefile' kernel/Makefile
-cp '$(CLION_PATH)/HW2/kernel/hw2.c' kernel/hw2.c
+cp '$(CLION_PATH)/arch/x86/entry/syscalls/syscall_64.tbl' arch/x86/entry/syscalls/syscall_64.tbl
+cp '$(CLION_PATH)/include/linux/init_task.h' include/linux/init_task.h
+cp '$(CLION_PATH)/include/linux/sched.h' include/linux/sched.h
+cp '$(CLION_PATH)/include/linux/syscalls.h' include/linux/syscalls.h
+cp '$(CLION_PATH)/kernel/Makefile' kernel/Makefile
+cp '$(CLION_PATH)/kernel/hw2.c' kernel/hw2.c
 
 echo '>> done.'
 echo ''
