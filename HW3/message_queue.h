2c4f28f16e8858c1e38117784d0dbbfe64e91498
amit <amitir22@gmail.com>
2021-06-06 19:47:19 +0000

    request.c:  - cosmetic changes server.c:  - added support for all the arguments parsing  - cosmetic changes thread_pool:  - added files, need to implement message_queue: - added files, need to implement

---
+++ HW3/message_queue.h
@@ -0,0 +1,6 @@
+#ifndef MESSAGE_QUEUE_H
+#define MESSAGE_QUEUE_H
+
+typedef struct t_message_queue *MessageQueue;
+
+#endif //MESSAGE_QUEUE_H
--- HW3/server.c
+++ HW3/server.c
@@ -1,6 +1,8 @@
 #include "segel.h"
 #include "request.h"
 
+#define SCHED_ALG_MAX_SIZE 7
+
 // 
 // server.c: A very, very simple web server
 //
@@ -12,29 +14,39 @@
 //
 
 // HW3: Parse the new arguments too
-void getargs(int *port, int *threadPoolSize,  int argc, char *argv[])
+void getargs(int *port, int *threadPoolSize, int *queueSize, char **schedAlg, int argc, char *argv[])
 {
     if (argc < 2) {
 	    fprintf(stderr, "Usage: %s <port>\n", argv[0]);
 	    exit(1);
     }
 
+    // TODO: maybe validate parameters?
     *port = atoi(argv[1]);
     *threadPoolSize = atoi(argv[2]);
+    *queueSize = atoi(argv[3]);
+
+    strcpy(*schedAlg, argv[4]);
 }
 
 
 int main(int argc, char *argv[])
 {
-    int listenfd, connfd, port, clientlen, threadPoolSize;
+    int listenfd, connfd, clientlen;
+    int port, threadPoolSize, queueSize;
+    char *schedAlgo = (char *) malloc(SCHED_ALG_MAX_SIZE);
+
     struct sockaddr_in clientaddr;
 
-    getargs(&port, &threadPoolSize, argc, argv);
+    getargs(&port, &threadPoolSize, &queueSize, &schedAlgo, argc, argv);
 
     // 
     // HW3: Create some threads...
     //
+    fprintf(stdout, "port: %d\n", port);
     fprintf(stdout, "thread pool size: %d\n", threadPoolSize);
+    fprintf(stdout, "queue size: %d\n", queueSize);
+    fprintf(stdout, "sched-algo: %s\n", schedAlgo);
 
     listenfd = Open_listenfd(port);
 
