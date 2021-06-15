e10a6c4c6720646b51716594a28bee268cbd4fbf
amit <amitir22@gmail.com>
2021-06-15 20:11:49 +0000

    implemented all. upgraded tests.

--- HW3/test.py
+++ HW3/test.py
@@ -6,7 +6,7 @@ THREAD_COUNT = 20
 ITERATION_COUNT = 1
 
 
-def send_requests():
+def send_dynamic_requests():
     for i in range(ITERATION_COUNT):
         to_print = ''
         to_print += str(current_thread()) + '\n'
@@ -18,11 +18,24 @@ def send_requests():
         print(to_print)
 
 
+def send_static_requests():
+    for i in range(ITERATION_COUNT):
+        to_print = ''
+        to_print += str(current_thread()) + '\n'
+        to_print += 'iteration: ' + str(i) + '\n'
+        response = run(args=('/home/student/CLionProjects/OperatingSystemWHW/HW3/client',
+                             'localhost', '4096', 'home.html'), stderr=STDOUT)
+        to_print += response.decode('utf-8')
+
+        print(to_print)
+
+
 def init_threads(amount):
     threads = []
 
     for i in range(amount):
-        threads.append(Thread(target=send_requests))
+        threads.append(Thread(target=send_dynamic_requests))
+        threads.append(Thread(target=send_static_requests))
 
     return threads
 
